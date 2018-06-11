850a919c50ffd9de34f5f76852be061c272b4e7d
Sambhav Kothari <sambhavs.email@gmail.com>
2018-06-12 00:56:35 +0530

    Increase default number of worker threads

--- benchmark.py
+++ benchmark.py
@@ -35,7 +35,7 @@ ENTITIES = {
 
 class LoadTester:
 
-    def __init__(self, log_path, prefix="http://195.201.149.141:8983/solr", max_workers=9, endpoint="advanced", percent=33):
+    def __init__(self, log_path, prefix="http://195.201.149.141:8983/solr", max_workers=20, endpoint="advanced", percent=33):
         self.log_path = log_path
         self.max_workers = max_workers
         self.prefix = prefix
