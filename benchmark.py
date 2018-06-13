3af376f9167cc5d0a2c8a23bc03102b9bcaafab5
Sambhav Kothari <sambhavs.email@gmail.com>
2018-06-13 14:30:13 +0530

    Print stats every 50k reqs

--- benchmark.py
+++ benchmark.py
@@ -35,7 +35,7 @@ ENTITIES = {
 
 class LoadTester:
 
-    def __init__(self, log_path, prefix="http://195.201.149.141:8983/solr", max_workers=20, endpoint="advanced", ratio=3):
+    def __init__(self, log_path, prefix="http://195.201.149.141:8983/solr", max_workers=20, endpoint="advanced", ratio=1):
         self.log_path = log_path
         self.max_workers = max_workers
         self.prefix = prefix
@@ -124,8 +124,10 @@ class LoadTester:
                 self.responses[entity][status] += 1
                 self.count += 1
                 ccount = self.count
-            if ccount % 100 == 0:
-                print("Completed {} requests".format(self.count))
+            if ccount % 50000 == 0:
+                self.print_stats()
+            elif ccount % 2000 == 0:
+                print("{} requests made.".format(ccount))
 
     def print_stats(self):
         if self.count:
